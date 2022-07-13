8ada9ac38975ba657dca456e261a98e8a90440fb
anshikamittal7 <anshikamittal34@gmail.com>
2022-07-13 12:50:30 +0530

    blurry loading

---
+++ P5 Blurry Loading/scripy.js
@@ -0,0 +1,13 @@
+let percentage = document.querySelector('.load');
+let bkg = document.querySelector('.bkg');
+let i = 0;
+let interval = setInterval(() => {
+    bkg.style.filter = `blur(${30 - (i / 2.5)}px)`;
+    percentage.innerHTML = `${i}%`
+    i++;
+    console.log(i);
+    if (i == 100) {
+        percentage.remove();
+        clearInterval(interval);
+    }
+}, 4)
