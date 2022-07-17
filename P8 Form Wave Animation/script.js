b4ce88cc32bdc92a13ca4f72b76a44adf4aa4de6
anshikamittal7 <anshikamittal34@gmail.com>
2022-07-17 22:30:08 +0530

    wave animation

--- P8 Form Wave Animation/script.js
+++ P8 Form Wave Animation/script.js
@@ -1,6 +1,5 @@
 
-const email = document.querySelector('.mail');
-const password = document.querySelector('.pw');
+
 const labels = document.querySelectorAll('.form-control label');
 
 labels.forEach(label => {
@@ -10,15 +9,4 @@ labels.forEach(label => {
         .map((letter, idx) => `<span id='span' style="transition-delay:${idx * 50}ms">${letter}</span>`).join('');
     console.dir(label);
 
-})
-let span = document.querySelectorAll('#span');
-email.addEventListener('click', () => {
-    for (let i = 0; i < 5; i++) {
-        span[i].classList.toggle('wave');
-    }
-})
-password.addEventListener('click', () => {
-    for (let i = 5; i < 13; i++) {
-        span[i].classList.toggle('wave');
-    }
 })
