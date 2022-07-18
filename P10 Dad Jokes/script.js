b04f9c14958348cf0a627fbe430d4aeeb04e2556
anshikamittal7 <anshikamittal34@gmail.com>
2022-07-18 13:46:44 +0530

    dad jokes

--- P10 Dad Jokes/script.js
+++ P10 Dad Jokes/script.js
@@ -1,9 +1,15 @@
 const btn = document.querySelector('button')
 btn.addEventListener('click', () => {
+
+    document.body.style.background = `linear-gradient(to right, ${colorGenerator()}, ${colorGenerator()})`
+});
+
+function colorGenerator() {
     let r = Math.floor(Math.random() * 256);
     let g = Math.floor(Math.random() * 256);
     let b = Math.floor(Math.random() * 256);
+    let color = `rgb(${r},${g},${b})`;
+    return color;
+}
 
-    document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
-});
 const url = 'GET https://icanhazdadjoke.com/';
