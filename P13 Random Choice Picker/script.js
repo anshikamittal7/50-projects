892f999fb295b810380fe4e9ae49a8dd0716c463
anshikamittal7 <anshikamittal34@gmail.com>
2022-07-25 02:21:06 +0530

    a

--- P13 Random Choice Picker/script.js
+++ P13 Random Choice Picker/script.js
@@ -1,1 +1,8 @@
 let str = '';
+document.addEventListener("keypress", function (event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+        let text = document.getElementById('text').value;
+        console.log(text);
+    }
+})
